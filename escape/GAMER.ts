22e48eb20a588611aa3f57fe6c430bc581486f9d
Sean <ussa.say@gmail.com>
2025-01-30 02:10:45 -0800

    ravensmith lore

--- escape/GAMER.ts
+++ escape/GAMER.ts
@@ -1,4 +1,38 @@
-let inventory: any[] = [];
+let inventory: string[] = [];
+
+/** 
+ * Each stage should be a PURE function (changes to game state should be done in
+ * choice functions, see below). The choices' functions don't need to pure
+ * (i.e., you can randomly choose what a choice does on every call)
+ */
+interface StageInfo {
+	/** 
+	 * Short description of where the player is. Displayed at the top and in the
+	 * tab title. Can use HTML.
+	 */
+	location: string
+	/**
+	 * Text shown before the choices. Can use HTML.
+	 */
+	description: string
+	/**
+	 * An object mapping from a string label (which can use HTML) to a function.
+	 * The function can either be
+	 * - another stage
+	 * - a function that returns a string
+	 *
+	 * Use the latter for any game state changes, like adding items to an
+	 * inventory. The returned string will be displayed (can use HTML) with a
+	 * single choice "ok" to return back to the same stage.
+	 *
+	 * `null` will hide the choice. This can be used to specify the order of the
+	 * choices in the object but programmatically show/hide them later in the
+	 * function.
+	 */
+	choices: {
+		[choiceLabel: string]: (() => StageInfo) | (() => string) | null
+	}
+}
 
 function BEGINNING() {
 	let description = "you stand in a desolate courtyard shrouded in fog. a cobblestone pathway surrounds a fountain, water dribbles meekly from a fish statue into the dark water. placards remain the only sign of where benches once stood, removed probably to discourage the homeless from sleeping here. the path continues north and south. ";
@@ -8,49 +42,86 @@ function BEGINNING() {
 	let choices = {
 		"go north": northPath,
 		"go south": southPath,
+		"go east": null as any,
 		'inspect fountain': rubberRoom1,
 	};
+	if (inventory.includes(mapItem)) {
+		choices["go east"] = () => {
+			return "the map says theres nothing to the east. <a href=\"https://github.com/Subset-UCSD/Commit-Challenge-2025/blob/main/escape/GAMER.ts\">for now...</a>"
+		}
+	}
 	return {
-		location: "courtyard",
+		location: inventory.includes(mapItem) ?"Ravensmith Court": "courtyard",
 		description,
 		choices,
 	};
 }
 
 const grassItem = "blade of grass\nIt's just a blade of grass.";
+let talkedToRaven = false
+let ravenCompassTaken = false
+const mapItem = "Ravensmith Estate map\na somewhat blurry photocopy of a map of Ravensmith's estate. scribbles and notes dot the map, but few are legible."
+const compassItem = "compass\ninvented by the chinese in 206 BCE."
 function northPath() {
 	let description = "the path trails off, leaving you standing on a field of uncut grass. ";
-	if (Math.random() < 0.5) {
-		let choices = {
-			"continue north": fieldMan,
-			"go south": BEGINNING,
-			"pick grass": () => {
-				inventory.push(grassItem)
-				return "you pluck a blade of grass from the ground. +1 blade of grass.";
-			}
-		};
-		return {
-			location: "field",
-			description,
-			choices,
-		};
+	let choices = {
+		"continue north": null as any,
+		"go south": BEGINNING,
+	};
+	if (inventory.includes(mapItem)) {
+		description += "empowered by the map, you feel ready to venture into the fog. "
+		choices[
+			"continue north"] = fieldMan
 	} else {
-		let choices = {
-			"continue north": fieldMan,
-			"go south": BEGINNING,
-			"pick grass": () => {
+		choices[
+			"continue north"] = () => {
+				return "you do not wish to stray from the path. in the fog, it is easy to get lost."
+			}
+	}
+	if (!talkedToRaven) {
+	description += "a raven dressed in a long, dark trenchcoat shivers in the cold. "
+		choices["talk to raven"] = () => {
+			talkedToRaven = true
+			inventory.push(mapItem)
+			return "the raven's name is Ravensmith. he apparently owns this property. since you're trespassing, he gives you a map for more accurate trespassery. +1 map"
+		}
+	} else {
+		description += "Ravensmith, dressed in a long, dark trenchcoat, shivers in the cold. "
+		if (!ravenCompassTaken) {
+			description += "a small, round object protrudes from his pocket. a compass, perhaps? "
+		}
+		if (!inventory.includes(sushiItem)||ravenCompassTaken) {
+				choices["talk to Ravensmith"] = () => {
+					return "Ravensmith does not bother with small talk. he humphs at you and turns his head away. "
+				}
+			} else {
+				choices["talk to Ravensmith"] = () => {
+					inventory.push(compassItem)
+					inventory.splice(inventory.indexOf(sushiItem), 1);
+					ravenCompassTaken = true;
+					return "you ask for his compass, offering a piece of sushi. he obliges. +1 compass "
+				}
+			}
+	}
+	if (Math.random() < 0.5) {
+		choices["pick grass"] = () => {
+			inventory.push(grassItem)
+			return "you pluck a blade of grass from the ground. +1 blade of grass.";
+		}
+	} else {
+		
+		choices["pick grass"] =() => {
 				let myDiv = document.getElementById("text-based-adventure")!;
 				myDiv.className = "GO_AWAY";
 				myDiv = document.getElementById("rpg-battle")!;
 				myDiv.className = "no-come-back-i-am-sorry";
-			}
 		};
-		return {
-			location: "field",
-			description,
-			choices,
-		};
-	}	
+	}  
+	return {
+		location: inventory.includes(mapItem)?"Unnamed Field on Ravensmith Estate":"field",
+		description,
+		choices,
+	};
 }
 
 const fishItem = "fish\na frozen fish wrapped in plastic on a styrofoam plate. its label says it's from winco.";
@@ -81,20 +152,20 @@ function southPath() {
 		description += "theres a sizeable hole on the wall where the poster once was. ";
 		// TODO: expand the game here!
 		choices["enter hole"] = () => {
-			return "the hole reveals a dead end. maybe there will be something on the other side in the future?";
+			return "the hole reveals a dead end. maybe there will be something on the other side <a href=\"https://github.com/Subset-UCSD/Commit-Challenge-2025/blob/main/escape/GAMER.ts\">in the future</a>?";
 		};
 	}
 	return {
-		location: "brick wall",
+		location: inventory.includes(mapItem) ? "Ravensmith Estate Wall": "brick wall",
 		description,
 		choices,
 	};
 }
 
-const sushiItem = `sushi piece\na slice of sushi. the man who made it seemed to be a professional sushi guy or whatever the word is. raw tuna wrapped in seaweed wrapped in sticky rice.`;
+const sushiItem = `sushi piece\na slice of sushi. the man who made it seemed to be a professional sushi guy or whatever the word is. raw tuna wrapped in seaweed wrapped in sticky rice. did you know? Ravensmith is a big fan of sushi.`;
 let manHungry = true;
 function fieldMan() {
-	let description = "you trudge on blindly into the endless grassland. suddenly, you spot a man, huddled in tattered clothes lying the ground. ";
+	let description = "you trudge on blindly into the endless grassland. suddenly, you spot a man, huddled in tattered clothes lying on the ground. ";
 	let choices = {
 		"return south": northPath,
 	};
@@ -113,7 +184,7 @@ function fieldMan() {
 		description += "he is soundly asleep, snoring a cacophony. ";
 	}
 	return {
-		location: "field",
+		location:inventory.includes(mapItem)?"Private Property - DO NOT TRESPASS!":  "field",
 		description,
 		choices,
 	};
@@ -121,7 +192,7 @@ function fieldMan() {
 
 function rubberRoom1() {
 	return {
-		location: 'courtyard',
+		location: inventory.includes(mapItem) ?"Ravensmith Court":'courtyard',
 		description: 'you slip and fall into the fountain. grasping for breath, you frantically try to paddle out, but feel yourself sinking further into the murky water.',
 		choices: {'fall': rubberRoom2}
 	};

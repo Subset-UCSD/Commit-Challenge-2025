4d94cce92c51cbf4b66391b326769c15be2be78b
Nick <nick-ls@users.noreply.github.com>
2025-02-06 01:03:33 -0800

    Normal

--- escape/renderer.ts
+++ escape/renderer.ts
@@ -8,53 +8,61 @@ declare let inventory: Inventory;
 const mod = { BEGINNING, inventory };
 let current = mod.BEGINNING;
 
-function escape(str:string) {
-	return str.replaceAll('&', '&amp;').replaceAll('<', '&lt;').replaceAll('>', '&gt;').replaceAll('"', '&quot;')
+function escape(str: string) {
+	return str.replaceAll("&", "&amp;").replaceAll("<", "&lt;").replaceAll(">", "&gt;").replaceAll(`"`, `&quot;`);
 }
 
-function renderItem({ name, lore }:Item) {
-	// const [name, lore = ''] = itemNameSpec.split('\n')
-	return `<span class="item ${lore ? 'has-lore' : ''}" >${name}${lore ? `<span class="lore"><span class="space"> </span>${lore}</span>` : ''}</span>`
+function renderItem({ name, lore }: Item) {
+	return `<span class="item ${lore ? "has-lore" : ""}" >${name}${
+		lore ? `<span class="lore"><span class="space"> </span>${lore}</span>` : ""
+	}</span>`;
 }
 
 function renderInventory() {
 	if (mod.inventory.size === 0) {
-		document.getElementById('inventory')!.innerHTML = ''
-		return
+		document.getElementById("inventory")!.innerHTML = "";
+		return;
 	}
-	document.getElementById('inventory')!.innerHTML = 'you have: ' + mod.inventory.counts().map(({ item, count }) => renderItem(item) + (count > 1 ? ` (${count})` : '')).join(', ')
+	document.getElementById("inventory")!.innerHTML = `you have: ${
+		mod.inventory.counts().map(({ item, count }) => 
+			renderItem(item) + (count > 1 ? ` (${count})` : "")
+		).join(", ")
+	}`;
 }
 
 const render = () => {
 	for (let i = 0; i < 87; i++) current() // call the stage 87 times to make sure it's pure
-	const { location, description, choices } = current()
-	document.title = location
-	document.getElementById('location')!.innerHTML = location
-	document.getElementById('description')!.innerHTML = description
-	document.getElementById('choices')!.innerHTML = Object.keys(choices).map((choice, i) => choices[choice] ? `<button onclick="select(${i})">${choice}</button>` : '').join('')
-	renderInventory()
+	const { location, description, choices } = current();
+	document.title = location;
+	document.getElementById("location")!.innerHTML = location;
+	document.getElementById("description")!.innerHTML = description;
+	document.getElementById("choices")!.innerHTML = Object.keys(choices).map(
+		(choice, i) => choices[choice] ? `<button onclick="select(${i})">${choice}</button>` : ""
+	).join("");
+	renderInventory();
 }
-const select = (index:number) => {
-	for (let i = 0; i < 169; i++) current() // call the stage 169 times to make sure it's pure
-	const { location, description, choices } = current()
+
+const select = (index: number) => {
+	for (let i = 0; i < 169; i++) current(); // call the stage 169 times to make sure it's pure
+	const { choices } = current();
 	const next: any = Object.values(choices)[index];
 	const result = next?.()
-	if (typeof result === 'string') {
+	if (typeof result === "string") {
 		// "choice thing": () => { do something; return "text to display" }
 		// something happened
-		document.getElementById('description')!.innerHTML = result
-		document.getElementById('choices')!.innerHTML = '<button onclick="render()">ok</button>'
+		document.getElementById("description")!.innerHTML = result
+		document.getElementById("choices")!.innerHTML = `<button onclick="render()">ok</button>`
 		renderInventory()
-	} else if (typeof result === 'function') {
+	} else if (typeof result === "function") {
 		// "choice thing": () => { do something; return nextStage }
 		// something happened
-		current = result
-		render()
+		current = result;
+		render();
 	} else if (result !== null) {
 		// "choice thing": nextStage
 		// it's probably a state and returned its info object
-		current = next
-		render()
+		current = next;
+		render();
 	}
 }
 render();
@@ -205,5 +213,5 @@ function load(state: Record<string, any>) {
 	for (const [k, v] of Object.entries(state)) {
 		(mod as Record<string, any>)[k] = v
 	}
-	render()
+	render();
 }
